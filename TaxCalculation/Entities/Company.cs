28d2905ee2e884dc206a57029e1ec3f6b14a1590
Gabriel Grabski <gabrielgrabski001@gmail.com>
2020-12-17 19:36:43 -0300

    Criação da estrutura do projeto

---
+++ TaxCalculation/Entities/Company.cs
@@ -0,0 +1,24 @@
+﻿namespace TaxCalculation.Entities
+{
+    class Company : TaxPayer
+    {
+        public int NumberOfEmployeers { get; set; }
+
+        public Company(string name, double anualIncome, int numberOfEmployeers) : base (name, anualIncome)
+        {
+            NumberOfEmployeers = numberOfEmployeers;
+        }
+
+        public override double CalculateTaxes()
+        {
+            if(NumberOfEmployeers > 10)
+            {
+                return AnualIncome * (16 / 100);
+            }
+            else
+            {
+                return AnualIncome * (14 / 100);
+            }
+        }
+    }
+}
---
+++ TaxCalculation/Entities/Individual.cs
@@ -0,0 +1,37 @@
+﻿namespace TaxCalculation.Entities
+{
+    class Individual : TaxPayer
+    {
+        public double HealthExpenditures { get; set; }
+
+        public Individual(string name, double anualIncome, double healthExpenditures) : base (name, anualIncome)
+        {
+        }
+
+        public override double CalculateTaxes()
+        {
+            if(AnualIncome < 2000)
+            {
+                if(HealthExpenditures != 0)
+                {
+                    return ((AnualIncome * (15/100)) - (HealthExpenditures * (50/100)));
+                } 
+                else
+                {
+                    return (AnualIncome * (15 / 100));
+                }
+            } 
+            else
+            {
+                if (HealthExpenditures != 0)
+                {
+                    return ((AnualIncome * (25 / 100)) - (HealthExpenditures * (50 / 100)));
+                }
+                else
+                {
+                    return (AnualIncome * (25 / 100));
+                }
+            }
+        }
+    }
+}
